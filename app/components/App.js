54b11e773e59ada7a10cbd7941cfacd4801b8ed1
Rafael Alonso <rafael.palonso@gmail.com>
2018-08-22 20:04:41 -0300

    Setup project and created first component

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default class App extends React.Component{
+	render(){
+		return (
+			<div id="header">
+				<h1>Hello World</h1>
+			</div>
+		)
+	}
+}
